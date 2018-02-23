5a2c537d944d6d8f3d86e450d9d3ae9359f24236
Cosh_ <cosh@slouple.com>
2018-02-22 23:01:56 -0700

    Added libraries

--- app/src/main/java/ca/ualberta/angrybidding/MainActivity.java
+++ app/src/main/java/ca/ualberta/angrybidding/MainActivity.java
@@ -3,6 +3,7 @@ package ca.ualberta.angrybidding;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.JsonElement;
+import com.slouple.android.VolleySingleton;
+import com.slouple.android.widget.button.SubmitButton;
+import com.slouple.android.widget.button.SubmitButtonListener;
+import com.slouple.lib.JsonRequest;
+import com.slouple.lib.JsonResponseListener;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +58,53 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        final SubmitButton testButton = findViewById(R.id.testSubmitButton);
+        testButton.setButtonListener(new SubmitButtonListener() {
+            @Override
+            public void onSubmit() {
+
+            }
+
+            @Override
+            public void onDisabledClick() {
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, "http://cosh.em.slouple.com:19200", null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        Log.d("MainActivity", "Request Success");
+                        Log.d("MainActivity", jsonObject.toString());
+                        final Snackbar snackbar = Snackbar.make(testButton, jsonObject.toString(), Snackbar.LENGTH_INDEFINITE);
+                        snackbar.setAction("Close", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                snackbar.dismiss();
+                            }
+                        });
+                        snackbar.show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Log.d("MainActivity", "Request Failed");
+                    }
+                }){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("Content-Type", "application/json");
+                        return params;
+                    }
+                };
+                VolleySingleton.getInstance(MainActivity.this).addToRequestQueue(request);
+
+
+            }
+
+            @Override
+            public void onSubmitClick() {
+
+            }
+        });
     }
 
     @Override
