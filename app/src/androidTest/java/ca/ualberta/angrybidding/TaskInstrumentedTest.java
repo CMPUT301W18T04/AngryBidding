ddb80b64bb9289ad610269e5df80a9b4b3196093
Cosh_ <cosh@slouple.com>
2018-02-23 18:39:21 -0700

    Fixed naming convention for DESCRIPTION

--- app/src/androidTest/java/ca/ualberta/angrybidding/TaskInstrumentedTest.java
+++ app/src/androidTest/java/ca/ualberta/angrybidding/TaskInstrumentedTest.java
@@ -15,39 +15,39 @@ import static org.junit.Assert.assertThat;
 public class TaskInstrumentedTest {
     public static final User USERNAME = new User("Lewis");
     public static final String TITLE = "This is the title for the task";
-    public static final String Description = "Description of the task";
+    public static final String DESCRIPTION = "Description of the task";
     public static final LocationPoint LOCATION_POINT = new LocationPoint(0,0);
     @Test
     public void newTask(){
         Task newTask = new Task(USERNAME, TITLE);
-        assertThat(newTask.getUser(),is(USERNAME));
-        assertThat(newTask.getTitle(),is(TITLE));
+        assertThat(newTask.getUser(), is(USERNAME));
+        assertThat(newTask.getTitle(), is(TITLE));
     }
 
     @Test
     public void newTaskDescription(){
-        Task newTask = new Task(USERNAME, TITLE,Description);
+        Task newTask = new Task(USERNAME, TITLE, DESCRIPTION);
         assertThat(newTask.getUser(), is(USERNAME));
         assertThat(newTask.getTitle(), is(TITLE));
-        assertThat(newTask.getDescription(),is(Description));
+        assertThat(newTask.getDescription(), is(DESCRIPTION));
     }
 
     @Test
     public void newTaskLocationPoint(){
-        Task newTask = new Task(USERNAME, TITLE,Description, LOCATION_POINT);
+        Task newTask = new Task(USERNAME, TITLE, DESCRIPTION, LOCATION_POINT);
         assertThat(newTask.getUser(), is(USERNAME));
         assertThat(newTask.getTitle(), is(TITLE));
-        assertThat(newTask.getDescription(),is(Description));
+        assertThat(newTask.getDescription(), is(DESCRIPTION));
         assertThat(newTask.getLocationPoint(), is(LOCATION_POINT));
     }
     @Test
     public void newTaskBids(){
         ArrayList<Bid> bids = new ArrayList<Bid>();
         bids.add(new Bid(new User("John"),80.32));
-        Task newTask = new Task(USERNAME, TITLE,Description, LOCATION_POINT,bids);
+        Task newTask = new Task(USERNAME, TITLE, DESCRIPTION, LOCATION_POINT,bids);
         assertThat(newTask.getUser(), is(USERNAME));
         assertThat(newTask.getTitle(), is(TITLE));
-        assertThat(newTask.getDescription(),is(Description));
+        assertThat(newTask.getDescription(), is(DESCRIPTION));
         assertThat(newTask.getLocationPoint(), is(LOCATION_POINT));
         assertThat(newTask.getBids(), is(bids));
     }
