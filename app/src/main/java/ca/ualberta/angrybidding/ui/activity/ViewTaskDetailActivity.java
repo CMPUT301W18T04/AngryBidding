bbb064f4a14bd8bb6f245b9e8f85a976dbba317f
zzxhelen <helenz1973@yahoo.ca>
2018-03-12 17:12:20 -0600

    View Task Details Activity implemented

--- app/src/main/java/ca/ualberta/angrybidding/ui/activity/ViewTaskDetailActivity.java
+++ app/src/main/java/ca/ualberta/angrybidding/ui/activity/ViewTaskDetailActivity.java
@@ -3,6 +3,7 @@ package ca.ualberta.angrybidding.ui.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.slouple.android.AdvancedActivity;
@@ -12,7 +13,11 @@ import ca.ualberta.angrybidding.R;
 
 public class ViewTaskDetailActivity extends AdvancedActivity {
 
-    ElasticSearchTask elasticSearchTask;
+    private ElasticSearchTask elasticSearchTask;
+    private TextView taskDetailTitle;
+    private TextView taskDetailOwner;
+    private TextView taskDetailDescription;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,17 @@ public class ViewTaskDetailActivity extends AdvancedActivity {
         String taskJson = intent.getStringExtra("task");
         elasticSearchTask = new Gson().fromJson(taskJson, ElasticSearchTask.class);
 
+        taskDetailTitle = findViewById(R.id.taskDetailTitle);
+        taskDetailOwner = findViewById(R.id.taskDetailOwner);
+        taskDetailDescription = findViewById(R.id.taskDetailDescription);
+
+        taskDetailTitle.setText(elasticSearchTask.getTitle());
+        taskDetailOwner.setText(elasticSearchTask.getUser().getUsername());
+        taskDetailDescription.setText(elasticSearchTask.getDescription());
+
     }
+
+
 
 
 }
