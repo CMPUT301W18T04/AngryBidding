505109f24a5db29b01d2ae058969df979103a833
NorOldBurden <NorOldBurden@gmail.com>
2018-03-18 05:04:00 -0600

    Updated EditTaskActivity UI

--- app/src/main/java/ca/ualberta/angrybidding/ui/activity/EditTaskActivity.java
+++ app/src/main/java/ca/ualberta/angrybidding/ui/activity/EditTaskActivity.java
@@ -2,106 +2,133 @@ package ca.ualberta.angrybidding.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.android.volley.VolleyError;
+import com.google.gson.Gson;
 import com.slouple.android.widget.button.SubmitButton;
+import com.slouple.android.widget.button.SubmitButtonListener;
+
+import java.math.BigDecimal;
 
 import ca.ualberta.angrybidding.ElasticSearchTask;
 import ca.ualberta.angrybidding.R;
 import ca.ualberta.angrybidding.elasticsearch.UpdateResponseListener;
-import ca.ualberta.angrybidding.ui.view.TaskView;
 
 /*
-This class is used to edit the tasks which are already created
+ * This class is used to edit the tasks which are already created
  */
 public class EditTaskActivity extends AngryBiddingActivity{
     public final static int REQUEST_CODE = 1004;
     private ElasticSearchTask currentTask;
+    private String currentID;
+    private EditText titleEditText;
+    private EditText descriptionEditText;
+    private SubmitButton editSaveButton;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_task);
 
-        currentTask = getIntent().
+        /*
+         * get intent pack of ElasticSearchTask object and ID
+         */
+        Intent pack = getIntent();
+        String taskJson = pack.getStringExtra("task");
+        currentID = pack.getStringExtra("id");
+        currentTask = new Gson().fromJson(taskJson, ElasticSearchTask.class);
 
+        titleEditText = findViewById(R.id.editTaskTitle);
+        descriptionEditText = findViewById(R.id.editTaskDescription);
+        editSaveButton = findViewById(R.id.editTaskSubmitButton);
 
-        final SubmitButton editSaveButton = findViewById(R.id.editTaskSave);
-        editSaveButton.setOnClickListener(new View.OnClickListener(){
+        titleEditText.setText(currentTask.getTitle());
+        descriptionEditText.setText(currentTask.getDescription());
 
+        TextWatcher textWatcher = new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                currentTask.setTitle(getEditTitle());
-                currentTask.setDescription(getEditDescription());
-                ElasticSearchTask.updateTask(this, currentID, currentTask, new UpdateResponseListener() {
-                    @Override
-                    public void onCreated(String id) {
-
-                    }
-
-                    @Override
-                    public void onUpdated(int version) {
-                        finish();
-                    }
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        editSaveButton.onError(R.string.errorOccurred);
-                    }
-                });
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
-        });
-
-        /*
-         * onClick action for delete button
-         */
-        final Button editDeleteButton = (Button) findViewById(R.id.editTaskDelete);
-        editDeleteButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
-            public void onClick(View v) {
-                if(canSubmit()){
-                    currentTask.delete()
-                    finish();
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (canSubmit()) {
+                    editSaveButton.enable();
                 } else {
-                    Toast.makeText(EditTaskActivity.this, "Error on save button",
-                            Toast.LENGTH_SHORT).show();
+                    editSaveButton.disable();
                 }
             }
+        };
+        titleEditText.addTextChangedListener(textWatcher);
+        descriptionEditText.addTextChangedListener(textWatcher);
+
+        /*
+         * Listener for save button
+         * Will save edited information after click
+         */
+        editSaveButton.setButtonListener(new SubmitButtonListener() {
+            @Override
+            public void onSubmit() {
+                EditTaskActivity.this.onSubmit();
+            }
+
+            @Override
+            public void onDisabledClick() {
+
+            }
+
+            @Override
+            public void onSubmitClick() {
+
+            }
         });
-
-        loadInfo(currentTask);
-
-    }
-
-    /*
-    load info(existed title and description for current task)
-     */
-    private void loadInfo(ElasticSearchTask task){
-        EditText editTitle = findViewById(R.id.editTaskTitle);
-        EditText editDescription = findViewById(R.id.editTaskDescription);
-
-        editTitle.setText(task.getTitle());
-        editDescription.setText(task.getDescription());
-    }
-
-    private boolean canSubmit(){
-        return true;
     }
 
     public String getEditTitle(){
-        EditText newTitle = findViewById(R.id.editTaskTitle);
-        return newTitle.getText().toString();
+        titleEditText = findViewById(R.id.editTaskTitle);
+        return titleEditText.getText().toString();
     }
 
     public String getEditDescription(){
-        EditText newDescription = findViewById(R.id.editTaskDescription);
-        return newDescription.getText().toString();
+        descriptionEditText = findViewById(R.id.editTaskDescription);
+        return descriptionEditText.getText().toString();
+    }
+
+    public boolean canSubmit(){
+        String title = titleEditText.getText().toString();
+        return title.length() >= 1;
+    }
+
+    public void onSubmit() {
+        currentTask.setTitle(getEditTitle());
+        currentTask.setDescription(getEditDescription());
+        ElasticSearchTask.updateTask(EditTaskActivity.this, currentID, currentTask, new UpdateResponseListener() {
+            @Override
+            public void onCreated(String id) {
+
+            }
+
+            @Override
+            public void onUpdated(int version) {
+                setResult(RESULT_OK);
+                finish();
+            }
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                editSaveButton.onError(R.string.errorOccurred);
+            }
+        });
+
     }
 
 }
