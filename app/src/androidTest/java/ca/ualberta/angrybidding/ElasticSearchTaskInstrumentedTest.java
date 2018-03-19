862afce9d46fd919096b65d5e72c499c277bdada
Cosh_ <cosh@slouple.com>
2018-03-19 08:10:15 -0600

    Update Unit Tests

---
+++ app/src/androidTest/java/ca/ualberta/angrybidding/ElasticSearchTaskInstrumentedTest.java
@@ -0,0 +1,33 @@
+package ca.ualberta.angrybidding;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public class ElasticSearchTaskInstrumentedTest {
+    public static final String ID = "abcde";
+    public static final User USERNAME = new User("Lewis");
+    public static final String TITLE = "This is the title for the task";
+    public static final String DESCRIPTION = "Description of the task";
+    public static final LocationPoint LOCATION_POINT = new LocationPoint(0, 0);
+
+    @Test
+    public void newTaskID() {
+        ArrayList<Bid> bids = new ArrayList<Bid>();
+        bids.add(new Bid(new User("John"), 80.32));
+        ElasticSearchTask newTask = new ElasticSearchTask(ID, USERNAME, TITLE, DESCRIPTION, LOCATION_POINT, bids);
+        assertThat(newTask.getUser(), is(USERNAME));
+        assertThat(newTask.getTitle(), is(TITLE));
+        assertThat(newTask.getDescription(), is(DESCRIPTION));
+        assertThat(newTask.getLocationPoint(), is(LOCATION_POINT));
+        assertThat(newTask.getBids(), is(bids));
+        assertThat(newTask.getID(), is(ID));
+    }
+}
--- app/src/androidTest/java/ca/ualberta/angrybidding/TaskInstrumentedTest.java
+++ app/src/androidTest/java/ca/ualberta/angrybidding/TaskInstrumentedTest.java
@@ -52,4 +52,55 @@ public class TaskInstrumentedTest {
         assertThat(newTask.getLocationPoint(), is(LOCATION_POINT));
         assertThat(newTask.getBids(), is(bids));
     }
+
+    @Test
+    public void taskSetTitle() {
+        ArrayList<Bid> bids = new ArrayList<Bid>();
+        bids.add(new Bid(new User("John"), 80.32));
+        Task newTask = new Task(USERNAME, TITLE, DESCRIPTION, LOCATION_POINT, bids);
+        String newTitle = "Hello World";
+        newTask.setTitle(newTitle);
+        assertThat(newTask.getTitle(), is(newTitle));
+    }
+
+    @Test
+    public void taskSetDescription() {
+        ArrayList<Bid> bids = new ArrayList<Bid>();
+        bids.add(new Bid(new User("John"), 80.32));
+        Task newTask = new Task(USERNAME, TITLE, DESCRIPTION, LOCATION_POINT, bids);
+        String newDescription = "QWER_AAAAAAAaaaa";
+        newTask.setDescription(newDescription);
+        assertThat(newTask.getDescription(), is(newDescription));
+    }
+
+    @Test
+    public void taskSetChosenBid() {
+        ArrayList<Bid> bids = new ArrayList<Bid>();
+        Bid bid = new Bid(new User("John"), 80.32);
+        bids.add(bid);
+        Task newTask = new Task(USERNAME, TITLE, DESCRIPTION, LOCATION_POINT, bids);
+        newTask.setChosenBid(bid);
+        assertThat(newTask.getBids().size(), is(0));
+        assertThat(newTask.getChosenBid().getUser().getUsername(), is(bid.getUser().getUsername()));
+        assertThat(newTask.getChosenBid().getPrice(), is(bid.getPrice()));
+    }
+
+    @Test
+    public void taskSetLocationPoint() {
+        ArrayList<Bid> bids = new ArrayList<Bid>();
+        bids.add(new Bid(new User("John"), 80.32));
+        Task newTask = new Task(USERNAME, TITLE, DESCRIPTION, LOCATION_POINT, bids);
+        LocationPoint locationPoint = new LocationPoint(50.5, 20.2);
+        newTask.setLocationPoint(locationPoint);
+        assertThat(newTask.getLocationPoint(), is(locationPoint));
+    }
+
+    @Test
+    public void taskSetCompleted() {
+        ArrayList<Bid> bids = new ArrayList<Bid>();
+        bids.add(new Bid(new User("John"), 80.32));
+        Task newTask = new Task(USERNAME, TITLE, DESCRIPTION, LOCATION_POINT, bids);
+        newTask.setCompleted(true);
+        assertThat(newTask.isCompleted(), is(true));
+    }
 }
