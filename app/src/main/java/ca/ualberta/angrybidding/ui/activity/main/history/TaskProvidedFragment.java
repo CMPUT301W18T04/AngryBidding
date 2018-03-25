38e424df1b32e7ea2b4548faf2fa548a4e19570f
NorOldBurden <NorOldBurden@gmail.com>
2018-03-24 22:00:45 -0600

    Updated TaskProvidedFragment

--- app/src/main/java/ca/ualberta/angrybidding/ui/activity/main/history/TaskProvidedFragment.java
+++ app/src/main/java/ca/ualberta/angrybidding/ui/activity/main/history/TaskProvidedFragment.java
@@ -1,5 +1,14 @@
 package ca.ualberta.angrybidding.ui.activity.main.history;
 
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.VolleyError;
+
+import java.util.ArrayList;
+
+import ca.ualberta.angrybidding.ElasticSearchTask;
+import ca.ualberta.angrybidding.ElasticSearchUser;
 import ca.ualberta.angrybidding.ui.fragment.TaskListFragment;
 
 /**
@@ -7,5 +16,33 @@ import ca.ualberta.angrybidding.ui.fragment.TaskListFragment;
  * Extends TaskListFragment for UI and basic functionality
  */
 public class TaskProvidedFragment extends TaskListFragment {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+    }
+
+    /**
+     * onRefresh() will update the task list and get the task list of current
+     * user from the elastic search server, and add the tasks to the task ArrayList
+     */
+    @Override
+    public void onRefresh() {
+        super.onRefresh();
+        ElasticSearchTask.listTaskByUser(getContext(), ElasticSearchUser.getMainUser(getContext()).getUsername(), new ElasticSearchTask.ListTaskListener() {
+            @Override
+            public void onResult(ArrayList<ElasticSearchTask> newTasks) {
+                tasks.addAll(newTasks);
+                recyclerView.getAdapter().notifyDataSetChanged();
+                finishRefresh();
+            }
+
+            /*
+             * Show message when a error occurs
+             */
+            @Override
+            public void onError(VolleyError error) {
+                Log.e("TaskPostedFragment", error.getMessage(), error);
+            }
+        });
+    }
 }
